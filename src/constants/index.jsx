df2e2eb1c6cb28039d66a22a1ec438b2661a2adf
FilippoBonafini <filippo.bonafini@outlook.com>
2023-10-13 11:55:46 +0200

    s

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -2,22 +2,6 @@ import { SocialMediaProfiles } from "@/components/SocialMedia";
 
 export const navigation = [
   {
-    title: "Progetti",
-    links: [
-      { title: "progetto 1", href: "/work/progetto1" },
-      { title: "progetto 2", href: "/work/progetto2" },
-      { title: "progetto 3", href: "/work/progetto3" },
-      {
-        title: (
-          <>
-            Vedi tutti <span aria-hidden="true">&rarr;</span>
-          </>
-        ),
-        href: "/work",
-      },
-    ],
-  },
-  {
     title: "Tulpa Studio",
     links: [
       { title: "Chi siamo", href: "/chi-siamo" },
