babaa9cc22cd274cef38c43e1f6143e2473292a0
FilippoBonafini <filippo.bonafini@outlook.com>
2023-10-17 10:52:30 +0200

    a

--- src/components/AddConferm.jsx
+++ src/components/AddConferm.jsx
@@ -15,18 +15,18 @@ function urlFor(source) {
 const AddConferm = ({ item }) => {
     return (
         <>
-            <div className="flex fixed bottom-3 right-6 bg-white rounded-lg border-gray-300 hover:border-blue-700 duration-300 border p-3 shadow-lg min-w-max gap-5">
+            <Link href={'/carrello'} className="flex fixed bottom-3 right-6 bg-white rounded-lg border-gray-300 hover:border-blue-700 duration-300 border p-3 shadow-lg min-w-max gap-5">
                 <div>
                     <Image loading='eager' width={80} height={80} src={urlFor(item.images[0]).width(80).height(80).url()} />
                 </div>
                 <div className='flex flex-col justify-around'>
                     <span className='text-black text-lg'>Aggiunto al carrello</span>
-                    <Button className='flex justify-center' href={'/carrello'}>
+                    <Button className='flex justify-center'>
                         Checkout
                     </Button>
                 </div>
 
-            </div>
+            </Link>
         </>
     )
 }
