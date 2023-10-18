fb9f2ad9a008f72051b860730dfebbe247dccea3
FilippoBonafini <filippo.bonafini@outlook.com>
2023-10-18 16:46:33 +0200

    add videos

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const PortfolioCard = ({ className, ...restProps }) => {
     return (
         <Link href={'portfolio/test'} className={`inline-block text-black shadow-md relative transition-transform transform hover:scale-[1.02] cursor-pointer h-[200px] overflow-hidden ${className}`} {...restProps}>
-            <video src="https://placehold.co/3840x2160.mp4?text=Hello+World"></video>
+            <video src="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" autoPlay loop></video>
         </Link>
     );
 }
