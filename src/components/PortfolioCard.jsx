7a1d1407482c63adb4f772ddf31f949b92c8dd69
FilippoBonafini <filippo.bonafini@outlook.com>
2023-10-19 17:16:32 +0200

    prova

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -2,11 +2,17 @@ import Link from "next/link";
 
 const PortfolioCard = ({ className, ...restProps }) => {
     return (
-        <Link href={'portfolio/test'} className={`inline-block text-black shadow-md relative transition-transform transform hover:scale-[1.02] cursor-pointer h-[200px] overflow-hidden ${className}`} {...restProps}>
-            <video src="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" autoPlay loop></video>
+        <Link href={'portfolio/test'} className="inline-block text-black shadow-md relative cursor-pointer h-200px overflow-hidden group" {...restProps}>
+            <div className="relative">
+                <img src="https://placehold.co/500x300" alt="test" />
+            </div>
+            <div className="absolute top-0 right-0 bottom-0  bg-blue-700 text-white p-4 w-[140px] group-hover:right-[-110px] transition-all duration-300 ease-in-out z-10 flex items-center">
+
+                <h2 className="text-2xl">Progetto di prova asdasda </h2>
+            </div>
+            <div className=" absolute top-0 right-6 bottom-0 bg-blue-700 text-white  w-[140px] group-hover:right-[-110px] group-hover:skew-x-0 transition-all duration-300 ease-in-out skew-x-12"></div>
         </Link>
     );
 }
 
 export default PortfolioCard;
-
