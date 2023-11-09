0341712379293723f1d78af76607ee3984427902
filippobonafini <filippo.bonafini@outlook.com>
2023-11-09 18:26:11 +0100

    develop branch

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+import image from "../images/editing-color-grading-suite.jpg"
 
 const PortfolioCard = ({ className, ...restProps }) => {
     return (
-        <Link href={'portfolio/test'} className="inline-block text-black shadow-md relative cursor-pointer h-200px overflow-hidden group" {...restProps}>
-            <div className="relative">
-                <img src="https://placehold.co/500x300" alt="test" />
+        <Link href={'portfolio/test'} className="inline-block text-black shadow-xl relative cursor-pointer h-200px overflow-hidden rounded-md group" {...restProps}>
+            <div className="relative ">
+                <Image className="scale-100 group-hover:scale-110 transition-all duration-1000" src={image} width={600} height={400} alt="image" />
             </div>
-            <div className="absolute top-0 right-0 bottom-0  bg-blue-700 text-white p-4 w-[140px] group-hover:right-[-110px] transition-all duration-300 ease-in-out z-10 flex items-center">
+            <div className="absolute top-0 right-0 bottom-0  bg-blue-700 text-white p-4 w-[130px] group-hover:right-[-110px] transition-all duration-300 ease-in-out z-10 flex items-center">
 
                 <h2 className="text-2xl">Progetto di prova asdasda </h2>
             </div>
--- src/components/PortfolioGrid.jsx
+++ src/components/PortfolioGrid.jsx
@@ -1,7 +1,7 @@
 import PortfolioCard from "./PortfolioCard"
 const PortfolioGrid = () => {
     return (
-        <div className="grid mt-14 lg:grid-cols-3 gap-4 md:grid-cols-2 sm:grid-cols-1 items-center">
+        <div className="grid mt-14 lg:grid-cols-2 gap-6 md:grid-cols-2 sm:grid-cols-1 items-center">
             <PortfolioCard />
             <PortfolioCard />
             <PortfolioCard />
