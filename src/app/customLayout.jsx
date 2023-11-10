1bdeb430f7456bc72bf69ac3c539bb76d31b4614
filippobonafini <filippo.bonafini@outlook.com>
2023-11-10 10:22:56 +0100

    e

--- src/app/customLayout.jsx
+++
@@ -1,9 +0,0 @@
-const CustomLayout = ({ children }) => (
-    <div className="custom-layout">
-        {/* Altri elementi del layout, come l'intestazione, la barra laterale, ecc. */}
-        {children}
-        {/* Altri elementi del layout, come il piè di pagina, ecc. */}
-    </div>
-);
-
-export default CustomLayout;
